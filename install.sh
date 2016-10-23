e7cb03dc45236a979832f3ee896ca0494ab9cf29
Broda Noel <noel@olapic.com>
2016-10-22 23:08:43 -0300

    Add bropy installer

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo 'BroPy installed properly'
+
+
+if [ -f "~/bropy/internal/installed.txt" ]
+then
+	echo 'Bropy is already installed.'
+	echo 'New version? You shuold run update.sh instead of install.sh'
+else
+	echo 'PATH=~/bropy/bin:$PATH' >> ~/.bash_profile
+	echo 'export PATH' >> ~/.bash_profile
+
+	mkdir ~/bropy/internal
+	echo `date +%s` >> ~/bropy/internal/installed.txt
+
+	echo 'BroPy installed properly'
+	echo 'Follow me! @BrodaNoel (same profile in all internet!)'
+	echo 'Give us your STAR in https://github.com/BrodaNoel/bropy ;)'
+fi
