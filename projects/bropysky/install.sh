b39bd644e321e4ef840788437062554985ed89bd
Broda Noel <noel@olapic.com>
2016-10-23 01:00:46 -0300

    Fix message

--- projects/bropysky/install.sh
+++ projects/bropysky/install.sh
@@ -9,11 +9,10 @@ sudo sed -i "s/^\($name\s*=\s*\).*\$/\1$value/" /boot/config.txt
 
 sudo systemctl stop serial-getty@ttyAMA0.service
 
-echo 'YOU MUST REBOOT THE RASPBERRY PI BEFORE CONTINUE!'
-
 # Al momento de ejecutar el RUN
 #echo 'stty -F /dev/ttyAMA0 9600 -brkint -imaxbel' >> ~/.bash_profile
 #echo 'sudo service gpsd stop' >> ~/.bash_profile
 #echo 'sudo gpsd -n /dev/ttyAMA0 -F /var/run/gpsd.sock' >> ~/.bash_profile
 
 echo 'BroPySky installed properly'
+echo 'YOU MUST REBOOT THE RASPBERRY PI BEFORE CONTINUE!'
