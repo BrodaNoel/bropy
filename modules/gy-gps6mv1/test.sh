4bf5697222d24aa3ffc656d869cc040332f304ef
Broda Noel <noel@olapic.com>
2016-10-27 00:15:19 -0300

    Fix test.sh

--- modules/gy-gps6mv1/test.sh
+++ modules/gy-gps6mv1/test.sh
@@ -4,19 +4,20 @@ echo 'Testing GY-GPS6MV1...'
 
 # Clean $1 (file to log the test)
 echo 'Cleaning temp file'
-echo '' > $1
+rm $1
+touch $1
 
 # Start module
 echo 'Starting services'
-$("`dirname $0`/core/start.sh")
+`dirname $0`/core/start.sh
 
 # Escribimos el dato en el archivo $1
 echo 'Geting and Writing data'
-$("`dirname $0`/get.sh") >> $1
+`dirname $0`/get.sh >> $1
 
 # Stop module
 echo 'Stopping services'
-$("`dirname $0`/core/stop.sh")
+`dirname $0`/core/stop.sh
 
 # Analizamos el dato escrito en el archivo $1
 echo 'Analyzing data'
