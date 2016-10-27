40312fec3ba3835d154a204738bc630670c5b043
Broda Noel <noel@olapic.com>
2016-10-27 01:10:52 -0300

    Add sudo

--- modules/gy-gps6mv1/core/start.sh
+++ modules/gy-gps6mv1/core/start.sh
@@ -2,6 +2,6 @@
 
 echo 'Starting GY-GPS6MV1 services'
 
-stty -F /dev/ttyAMA0 9600 -brkint -imaxbel
+sudo stty -F /dev/ttyAMA0 9600 -brkint -imaxbel
 sudo service gpsd stop
 sudo gpsd -n /dev/ttyAMA0 -F /var/run/gpsd.sock
