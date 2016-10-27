80511fac570e46a023a36a9b4d9fa0d1fd330279
Broda Noel <noel@olapic.com>
2016-10-27 00:05:23 -0300

    Clean project

--- projects/bropysky/install.sh
+++ projects/bropysky/install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#
-# 1 sola vez
-sudo apt-get install -y gpsd
-
-name='enable_uart'
-value='0'
-sudo sed -i "s/^\($name\s*=\s*\).*\$/\1$value/" /boot/config.txt
-
-sudo systemctl stop serial-getty@ttyAMA0.service
 
 # Al momento de ejecutar el RUN
 #echo 'stty -F /dev/ttyAMA0 9600 -brkint -imaxbel' >> ~/.bash_profile
