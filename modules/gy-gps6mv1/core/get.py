09ca217d1b15cca160589e0fc7aea069babab670
Broda Noel <noel@olapic.com>
2016-11-01 00:50:11 -0300

    Fix multiples error in get.py

--- modules/gy-gps6mv1/core/get.py
+++ modules/gy-gps6mv1/core/get.py
@@ -34,45 +34,40 @@ if __name__ == '__main__':
     while not gotData and attempts < 3:
       #It may take a second or two to get good data
 
+      # TODO What it we get a "nan"?
       if gpsd.fix.latitude != 0 or gpsd.fix.longitude != 0:
         gotData = True
         attempts += 1
 
-        output = '{'
+        output = '{"status": "ok", "data": '
 
         output += '{'
-        output += 'latitude:', gpsd.fix.latitude, ','
-        output += 'longitude:', gpsd.fix.longitude, ','
-        output += 'time:"' + gpsd.fix.time + '",'
-        output += 'utcTime:"' + gpsd.utc + '",'
-        output += 'altitude:', gpsd.fix.altitude, ','
-        output += 'eps:', gpsd.fix.eps, ','
-        output += 'epx:', gpsd.fix.epx, ','
-        output += 'epv:', gpsd.fix.epv, ','
-        output += 'ept:', gpsd.fix.ept, ','
-        output += 'speed:', gpsd.fix.speed, ','
-        output += 'climb:', gpsd.fix.climb, ','
-        output += 'track:', gpsd.fix.track, ','
-        output += 'mode:', gpsd.fix.mode
-        #print 'satellites:', gpsd.satellites
-        output += '}'
+        output += '"latitude":' + str(gpsd.fix.latitude) + ','
+        output += '"longitude":' + str(gpsd.fix.longitude) + ','
+        output += '"time":"' + str(gpsd.fix.time) + '",'
+        output += '"utcTime":"' + str(gpsd.utc) + '",'
+        output += '"altitude":' + str(gpsd.fix.altitude) + ','
+        output += '"eps":' + str(gpsd.fix.eps) + ','
+        output += '"epx":' + str(gpsd.fix.epx) + ','
+        output += '"epv":' + str(gpsd.fix.epv) + ','
+        output += '"ept":' + str(gpsd.fix.ept) + ','
+        output += '"speed":' + str(gpsd.fix.speed) + ','
+        output += '"climb":' + str(gpsd.fix.climb) + ','
+        output += '"track":' + str(gpsd.fix.track) + ','
+        output += '"mode":' + str(gpsd.fix.mode)
+        #output += 'satellites:' + str(gpsd.satellites)
+        output += '} }'
 
         sys.stdout.write(output)
-        
-        sys.exit(0)
 
       else:
         time.sleep(1) #set to whatever
 
     if not gotData:
-      sys.exit(1)
+      sys.stdout.write('{ "status": "error", "message": "Too much attempts" }')
 
-  except: #when you press ctrl+c
-    try:
-      print("Unexpected error (1):", sys.exc_info()[0])
-      gpsp.running = False
-      gpsp.join() # wait for the thread to finish what it's doing
+  except:
+    sys.stdout.write('{"status": "error", "message": "' + str(sys.exc_info()) + '"}')
 
-    except:
-      print("Unexpected error (2):", sys.exc_info()[0])
-      sys.exit(1)
+gpsp.running = False
+gpsp.join() # wait for the thread to finish what it's doing
