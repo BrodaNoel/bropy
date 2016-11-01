afc2086d44b14a97ea0438055de60dc0934bf672
Broda Noel <noel@olapic.com>
2016-10-31 23:14:55 -0300

    New stay config

--- modules/gy-gps6mv1/core/start.sh
+++ modules/gy-gps6mv1/core/start.sh
@@ -2,6 +2,7 @@
 
 echo 'Starting GY-GPS6MV1 services'
 
-sudo stty -F /dev/ttyAMA0 9600 -brkint -imaxbel
+# sudo stty -F /dev/ttyAMA0 9600 -brkint -imaxbel
+sudo stty -F /dev/ttyAMA0 500:5:cbd:8a3b:3:1c:7f:15:4:0:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0
 sudo service gpsd stop
 sudo gpsd -n /dev/ttyAMA0 -F /var/run/gpsd.sock
