698e6faa14ea847cee251cd0465ee693c93fb445
Broda Noel <noel@olapic.com>
2016-11-02 00:53:36 -0300

    Test file

---
+++ modules/gy-gps6mv1/core/test.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+import json
+
+f = open('./../internals/test_data.json', 'r')
+response = json.loads(f.read())
+
+if response.status === 'ok':
+	print 'Everything is working well. Good news!'
+else:
+	print "Looks like it's not working. Bad news!"
--- modules/gy-gps6mv1/test.sh
+++ modules/gy-gps6mv1/test.sh
@@ -14,6 +14,7 @@ $DIR/get.sh > $DIR/internals/test_data.json
 
 # Analizamos el dato escrito en el archivo $1
 echo 'Analyzing data'
+python $DIR/core/test.py
 
 # Damos el reporte del analisis (funcionó bien o mal)
 
