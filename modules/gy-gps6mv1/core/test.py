554af8833bcb3cb4c9b5b1d2caa7161083278409
Broda Noel <noel@olapic.com>
2016-11-02 00:54:13 -0300

    Come on man...

--- modules/gy-gps6mv1/core/test.py
+++ modules/gy-gps6mv1/core/test.py
@@ -4,7 +4,7 @@ import json
 f = open('./../internals/test_data.json', 'r')
 response = json.loads(f.read())
 
-if response.status === 'ok':
+if response.status == 'ok':
 	print 'Everything is working well. Good news!'
 else:
 	print "Looks like it's not working. Bad news!"
