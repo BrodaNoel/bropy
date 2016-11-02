1fc09f7bfb51a5a8742eab133dda2c5953e96176
Broda Noel <noel@olapic.com>
2016-11-02 01:00:45 -0300

    I want to cry

--- modules/gy-gps6mv1/core/test.py
+++ modules/gy-gps6mv1/core/test.py
@@ -5,7 +5,7 @@ import os
 f = open(os.path.dirname(os.path.realpath(__file__)) + '/../internals/test_data.json', 'r')
 response = json.loads(f.read())
 
-if response.status == 'ok':
+if response['status'] == 'ok':
 	print 'Everything is working well. Good news!'
 else:
 	print "Looks like it's not working. Bad news!"
