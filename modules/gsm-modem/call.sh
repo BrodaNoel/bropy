2c15a936da89f9bbc4a5f383248a22eacb776860
Broda Noel <noel@olapic.com>
2016-11-08 20:22:41 -0300

    Implement gammu.config file

--- modules/gsm-modem/call.sh
+++ modules/gsm-modem/call.sh
@@ -3,6 +3,6 @@
 echo "Calling GSM-MODEM call.sh with arguments: $1 and $2 and $3"
 
 # bropy call module gsm-modem sendsms "phone-number" "text"
-
-sudo gammu $1 TEXT $2 -text $3
+DIR=`dirname $0`
+gammu -c $DIR/gammu.config $1 TEXT $2 -text $3
 exit $?
--- modules/gsm-modem/install.sh
+++ modules/gsm-modem/install.sh
@@ -3,15 +3,4 @@
 # Install dependencies
 sudo apt-get install -y gammu
 
-sudo touch /root/.gammurc
-sudo echo '[gammu]' >> /root/.gammurc
-sudo echo 'port = /dev/ttyUSB0' >> /root/.gammurc
-sudo echo 'model = ' >> /root/.gammurc
-sudo echo 'connection = at19200' >> /root/.gammurc
-sudo echo 'synchronizetime = yes' >> /root/.gammurc
-sudo echo 'logfile = ' >> /root/.gammurc
-sudo echo 'logformat = nothing' >> /root/.gammurc
-sudo echo 'use_locking = ' >> /root/.gammurc
-sudo echo 'gammuloc = ' >> /root/.gammurc
-
-exit 0
+exit $?
