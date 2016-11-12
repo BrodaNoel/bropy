e5c3f806af7c34b61e66aeba3bddcd8aba7617b1
Broda Noel <noel@olapic.com>
2016-11-12 01:55:59 -0300

    Fix gsm parameter with spaces

--- modules/gsm-modem/call.sh
+++ modules/gsm-modem/call.sh
@@ -2,5 +2,5 @@
 
 # bropy call module gsm-modem sendsms "phone-number" "text"
 DIR=`dirname $0`
-gammu -c $DIR/gammu.config $1 TEXT $2 -text $3
+gammu -c $DIR/gammu.config $1 TEXT $2 -text "$3"
 exit $?
